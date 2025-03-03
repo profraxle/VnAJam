b2c13e26cb9096078546ae8845ed7549caa72635
Kushagra <syashu31+github@gmail.com>
2025-03-03 11:47:10 +0000

    fishing behaviour started

--- Assets/Scripts/HuntingBehaviour.cs
+++ Assets/Scripts/HuntingBehaviour.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class FishingBehaviour : BaseBehaviour
 {
+    KeyCode[] actionKeysSet = { 
+        KeyCode.W,
+        KeyCode.A,
+        KeyCode.S,
+        KeyCode.D
+    };
+
+    KeyCode[] actionKeys;
+
+    KeyCode[] curActions;
+    int curIndex = 0;
+
+    public bool isFishing = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,52 @@ public class FishingBehaviour : BaseBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFishing)
+        {
+            if (Input.GetKeyDown(actionKeys[curIndex]))
+            {
+                if(curIndex == actionKeys.Length - 1)
+                {
+                    isFishing = false;
+                    FishingDoneSuccess();
+                }
+                else
+                {
+                    curActions[curIndex] = actionKeys[curIndex];
+                    curIndex++;
+                }
+            }
+        }
+    }
+
+    void FishingDoneSuccess()
+    {
+        Debug.LogError("Fishing was Successful");
+    }
+
+    void GenerateRandomActions(int totActions)
+    {
+        actionKeys = new KeyCode[totActions];
+        for (int i=0; i < totActions; i++)
+        {
+            int index = Random.Range(0, actionKeysSet.Length);
+            actionKeys[i] = actionKeysSet[index];
+        }
+    }
+
+    public void StartFishing(int totActions)
+    {
+        isFishing = true;
+        GenerateRandomActions(totActions);
+
+        PrintActions();
+    }
+
+    void PrintActions()
+    {
+        foreach (KeyCode action in actionKeys)
+        {
+            Debug.Log("Press: " + action);
+        }
     }
 }
