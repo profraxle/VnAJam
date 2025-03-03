8b6f0c483afeb45b1897e497b01e71205dd17c96
Kushagra <syashu31+github@gmail.com>
2025-03-03 15:39:39 +0000

    added timer

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager instance;
+
+    [Header("UI")]
+    public TextMeshProUGUI timeText;
+    [Space(10)]
+
+    [Header("Time Variables")]
+    [SerializeField] bool start = false;
+
+    float curTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start)
+        {
+            curTime += Time.deltaTime;
+            ShowTime();
+        }
+    }
+
+    void ShowTime()
+    {
+        int minutes = Mathf.FloorToInt(curTime / 60F);
+        int seconds = Mathf.FloorToInt(curTime - minutes * 60);
+
+        string niceTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        timeText.text = "Time: " + niceTime;
+    }
+
+    public void GetCurTime()
+    {
+
+    }
+}
